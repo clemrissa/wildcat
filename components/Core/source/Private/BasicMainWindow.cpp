a7bf63d5119b7d1cf9b69206c839cdc485a185e0
Dmitry Pinaev <dimitry.pinaev@gmail.com>
2014-06-14 14:31:59 +0200

    Clean up debug output

--- components/Core/source/Private/BasicMainWindow.cpp
+++ components/Core/source/Private/BasicMainWindow.cpp
@@ -16,63 +16,65 @@ using Geo::Core::Private::BasicMainWindow;
 
 BasicMainWindow::
 BasicMainWindow():
-  _mdiArea(new QMdiArea(this)) {
-  INFO << "BasicMainWindow is being created";
-
+  _mdiArea(new QMdiArea(this))
+{
   setCentralWidget(_mdiArea);
 
   showMaximized();
 }
 
+
 BasicMainWindow::
-~BasicMainWindow() {
-  INFO << "BasicMainWindow is being destroyed";
+~BasicMainWindow()
+{
+  //
 }
+
 
 Q_INVOKABLE void
 BasicMainWindow::
-toBottomDock(QDockWidget* dockWidget) {
-  DEBUG << "BasicMainWindow::toBottomDock(QDockWidget* dockWidget) is being invoked";
-
+toBottomDock(QDockWidget* dockWidget)
+{
   addDockWidget(Qt::BottomDockWidgetArea, dockWidget);
 }
 
+
 Q_INVOKABLE void
 BasicMainWindow::
-toLeftDock(QDockWidget* dockWidget) {
-  DEBUG << "BasicMainWindow::toLeftDock(QDockWidget* dockWidget) is being invoked";
-
+toLeftDock(QDockWidget* dockWidget)
+{
   addDockWidget(Qt::LeftDockWidgetArea, dockWidget);
 }
 
+
 Q_INVOKABLE void
 BasicMainWindow::
-toRightDock(QDockWidget* dockWidget) {
-  DEBUG << "BasicMainWindow::toRightDock(QDockWidget* dockWidget) is being invoked";
-
+toRightDock(QDockWidget* dockWidget)
+{
   addDockWidget(Qt::RightDockWidgetArea, dockWidget);
 }
 
+
 Q_INVOKABLE void
 BasicMainWindow::
-toCentralWidget(QWidget* widget) {
-  DEBUG << "BasicMainWindow::toCentralWidget(QWidget* dockWidget) is being invoked";
-
+toCentralWidget(QWidget* widget)
+{
   QMdiSubWindow* subWindow = _mdiArea->addSubWindow(widget);
   subWindow->show();
 }
 
-Q_INVOKABLE void
-BasicMainWindow::
-addMenu(QMenu* menu) {
-  DEBUG << "BasicMainWindow::addMenu(QMenu* menu) is invoked";
-  menuBar()->addMenu(menu);
-  // menuBar()->show();
-}
 
 Q_INVOKABLE void
 BasicMainWindow::
-addToolBar(QToolBar* toolBar) {
-  DEBUG << "BasicMainWindow::addToolBar(QToolBar* toolBar) is invoked";
+addMenu(QMenu* menu)
+{
+  menuBar()->addMenu(menu);
+}
+
+
+Q_INVOKABLE void
+BasicMainWindow::
+addToolBar(QToolBar* toolBar)
+{
   QMainWindow::addToolBar(toolBar);
 }
